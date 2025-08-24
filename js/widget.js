0e67ad04ae7358307455c659a8b7252e55b1cfcc
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-23 23:11:32 -0400

    filter and display simulated JSON data

--- js/widget.js
+++ js/widget.js
@@ -1,3 +1,16 @@
+/* --- Steps to work through
+Processing JSON Data
+
+1. Create a new HTML list item
+statusHTML
+
+2. Check the "inoffice" property
+
+3. Get the value for the "name" property; insert it inside the <li> tag
+
+4. Close the <li> tag
+*/
+
 // 1. create an XMLHTTP Request Object
 const xhr = new XMLHttpRequest()
 
@@ -5,6 +18,17 @@ const xhr = new XMLHttpRequest()
 xhr.onreadystatechange = () => {
   if (xhr.readyState === 4) {
     const employees = JSON.parse(xhr.responseText)
+    let statusHTML = '<ul class="bulleted">'
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].inoffice === true) {
+        statusHTML += '<li class="in">'
+      } else {
+        statusHTML += '<li class="out">'
+      }
+      statusHTML += employees[i].name + '</li>'
+    }
+    statusHTML += '</ul>'
+    document.getElementById('employeeList').innerHTML = statusHTML
   }
 }
 
